537b4c82921b92478a8a68a7c18f43b2a7ae5697
AS Sayem <andsayem@gmail.com>
2021-12-09 15:41:43 +0600

    Update (Routers)

--- routes.js
+++ routes.js
@@ -1,10 +1,19 @@
 var express = require("express");
 var router = express.Router();
-
+const axios = require("axios");
 router.get("/", function(req, res) {
+    axios.get("https://api.neoscan.io/api/main_net/v1/get_all_nodes")
+        .then(data => res.json(data))
+        .catch(err => res.secn(err))
+    res.render("user/login");
+});
+router.get("/home", function(req, res) {
     res.render("index");
 });
-router.get("/login", function(req, res) {
-    res.render("user/login");
+router.get("/dashboard-ecommerce", function(req, res) {
+    res.render("dashboard-ecommerce");
+});
+router.get("/dashboard-analytics", function(req, res) {
+    res.render("dashboard-analytics");
 });
 module.exports = router;
